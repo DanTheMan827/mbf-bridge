cc51f422ea7f0612759d1526b779917fd5f982fb
Daniel <790119+DanTheMan827@users.noreply.github.com>
2025-03-15 19:28:06 -0500

    MBF Bridge changes

--- build.sh
+++ build.sh
@@ -10,12 +10,12 @@ for arch in $archs; do
   SDKROOT=$(xcrun -sdk $sdk_version --show-sdk-path) \
     MACOSX_DEPLOYMENT_TARGET=$(xcrun -sdk $sdk_version --show-sdk-platform-version) \
       cargo build --target $target --release
-  cp -r tango-bridge.app target/$target/release
-  cp target/$target/release/tango_bridge_rs target/$target/release/tango-bridge.app/Contents/MacOS/tango-bridge
+  cp -r mbf-bridge.app target/$target/release
+  cp target/$target/release/mbf_bridge target/$target/release/mbf-bridge.app/Contents/MacOS/tango-bridge
   mkdir -p bin/mac/$arch
   rm -f bin/mac/$arch/tango-bridge.zip
   pushd target/$target/release
-  zip -r ../../../bin/mac/$arch/tango-bridge.zip tango-bridge.app
+  zip -r ../../../bin/mac/$arch/tango-bridge.zip mbf-bridge.app
   popd
 done
 
@@ -23,13 +23,13 @@ mv bin/mac/x86_64 bin/mac/x86-64
 
 arch="universal"
 mkdir -p target/$arch/release
-cp -r tango-bridge.app target/$arch/release
+cp -r mbf-bridge.app target/$arch/release
 lipo -create \
-  -output target/$arch/release/tango-bridge.app/Contents/MacOS/tango-bridge \
-  target/x86_64-apple-darwin/release/tango_bridge_rs \
-  target/aarch64-apple-darwin/release/tango_bridge_rs
+  -output target/$arch/release/mbf-bridge.app/Contents/MacOS/tango-bridge \
+  target/x86_64-apple-darwin/release/mbf_bridge \
+  target/aarch64-apple-darwin/release/mbf_bridge
 mkdir -p bin/mac/$arch
 rm -f bin/mac/$arch/tango-bridge.zip
 pushd target/$arch/release
-zip -r ../../../bin/mac/$arch/tango-bridge.zip tango-bridge.app
+zip -r ../../../bin/mac/$arch/tango-bridge.zip mbf-bridge.app
 popd
--- src/main.rs
+++ src/main.rs
@@ -27,7 +27,7 @@ use tokio::{
     net::TcpStream,
     process::Command,
 };
-use tower_http::cors::CorsLayer;
+use tower_http::cors::{AllowOrigin, CorsLayer};
 use tray_icon::{
     menu::{CheckMenuItem, Menu, MenuEvent, MenuItem, PredefinedMenuItem},
     TrayIconBuilder, TrayIconEvent,
@@ -138,7 +138,7 @@ async fn adb_connect_or_start() -> tokio::io::Result<TcpStream> {
 }
 
 fn start_browser() {
-    open::that_detached("https://app.tangoapp.dev/?desktop=true").unwrap();
+    open::that_detached("https://mbf.bsquest.xyz/").unwrap();
 }
 
 async fn handle_websocket(ws: WebSocket) {
@@ -175,49 +175,6 @@ async fn handle_websocket(ws: WebSocket) {
 }
 
 const ARG_AUTO_RUN: &str = "--auto-run";
-
-#[cfg(debug_assertions)]
-const PROXY_HOST: &str = "https://app.tangoapp.dev";
-#[cfg(not(debug_assertions))]
-const PROXY_HOST: &str = "https://app.tangoapp.dev";
-
-static CLIENT: OnceLock<reqwest::Client> = OnceLock::new();
-
-#[axum::debug_handler]
-async fn proxy_request(request: Request) -> Result<Response, Response> {
-    println!("proxy_request: {} {}", request.method(), request.uri());
-
-    let url = Url::options()
-        .base_url(Some(&Url::parse(PROXY_HOST).unwrap()))
-        .parse(&request.uri().to_string())
-        .map_err(|_| (StatusCode::BAD_REQUEST, "Bad Request").into_response())?;
-
-    let mut headers = request.headers().clone();
-    headers.insert("Host", url.host_str().unwrap().parse().unwrap());
-
-    let (client, request) = CLIENT
-        .get_or_init(|| reqwest::Client::new())
-        .request(request.method().clone(), url)
-        .headers(headers)
-        .body(reqwest::Body::wrap_stream(
-            request.into_body().into_data_stream(),
-        ))
-        .build_split();
-
-    let request = request.map_err(|_| (StatusCode::BAD_REQUEST, "Bad Request").into_response())?;
-
-    let response = client
-        .execute(request)
-        .await
-        .map_err(|_| (StatusCode::BAD_GATEWAY, "Bad Gateway").into_response())?;
-
-    Ok((
-        response.status(),
-        response.headers().clone(),
-        axum::body::Body::new(reqwest::Body::from(response)),
-    )
-        .into_response())
-}
 
 static SINGLE_INSTANCE: OnceLock<single_instance::SingleInstance> = OnceLock::new();
 
@@ -256,18 +213,15 @@ async fn main() {
                         .allow_methods([Method::GET, Method::POST])
                         .allow_origin(
                             [
-                                "http://localhost:3002",
-                                "https://app.tangoapp.dev",
-                                "https://beta.tangoapp.dev",
-                                "https://tunnel.tangoapp.dev",
+                                "http://localhost:3000",
+                                "https://localhost:3000",
+                                "https://mbf.bsquest.xyz",
                             ]
-                            .map(|x| x.parse().unwrap()),
-                        )
+                            .map(|x| x.parse().unwrap()))
                         .allow_private_network(true),
                 ),
-        )
-        .fallback(proxy_request);
-    let listener = tokio::net::TcpListener::bind("0.0.0.0:15037")
+        );
+    let listener = tokio::net::TcpListener::bind("0.0.0.0:25037")
         .await
         .unwrap();
 
